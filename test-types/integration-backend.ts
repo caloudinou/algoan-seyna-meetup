10bab53cb81c9406ad42291dc452970a32b8e9a0
caloudinou <pascalou@gmail.com>
2022-11-16 14:06:17 +0100

    fix(test-types): example

--- test-types/integration-backend.ts
+++ test-types/integration-backend.ts
@@ -1,14 +1,14 @@
 // integration tests typically only mock HTTP requests via MSW or Mirage
 const server = setupServer(...handlers)
 
-beforeAll(() => { server.listen(); app.listen(); })
+beforeAll(() => { server.listen(); })
 
-afterAll(() => { server.close(); app.close(); })
+afterAll(() => { server.close(); })
 
 afterEach(() => server.resetHandlers())
 
 test(`GET all products`, async () => {
-    await app.ready();
+    runApp(config);
 
     const response = await fetch(`http://localhost:${port}/2022-11/products/`);
     const data = await response.json();
